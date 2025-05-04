e47543dd2655a3d662967235ee86249d8870eee5
LeeWWilliamson <104240469+LeeWWilliamson@users.noreply.github.com>
2025-05-03 21:16:47 -0700

    Enforce TLS and node 18.x

--- W02/db/connect.js
+++ W02/db/connect.js
@@ -1,5 +1,47 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+
+  MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+    tls: true, // Ensure TLS is enabled
+  })
+    .then((client) => {
+      _db = client;
+      console.log('MongoDB connection successful');
+      callback(null, _db);
+    })
+    .catch((err) => {
+      console.error('MongoDB connection failed', err);
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
+
+
+/* const dotenv = require('dotenv');
+dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 
 let _db;
@@ -30,3 +72,4 @@ module.exports = {
   initDb,
   getDb,
 };
+ */
