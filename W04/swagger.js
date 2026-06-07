6903c6728f7419f8221c835f5c072c187961aa8c
LeeWWilliamson <104240469+LeeWWilliamson@users.noreply.github.com>
2026-06-06 21:51:30 -0700

    Added swagger, Render URL and https

---
+++ W04/swagger.js
@@ -0,0 +1,12 @@
+const swaggerAutogen = require('swagger-autogen')();
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API'
+  },
+  host: 'w04.onrender.com',
+  schemes: ['https']
+};
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+swaggerAutogen(outputFile, endpointsFiles, doc);
